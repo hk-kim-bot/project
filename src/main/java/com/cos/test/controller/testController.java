e1b540e4fda75de3d03c52e152e380e351767d5d
kimhyunhyu <khf5821@naver.com>
2022-12-16 10:10:33 +0900

    upload data

---
+++ src/main/java/com/cos/test/controller/testController.java
@@ -0,0 +1,42 @@
+package com.cos.test.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class testController {
+
+	@GetMapping("/user/login")
+	public String login() {
+		return "user/login";
+	}
+	@GetMapping("/user/join")
+	public String join() {
+		return "user/join";
+	}
+	@GetMapping("/board/csboard")
+	public String csboard() {
+		return "board/cs";
+	}
+	@GetMapping("/main")
+	public String main() {
+		return "index";
+	}
+	@GetMapping("/user/mypage")
+	public String mypage() {
+		return "user/mypage";
+	}
+	@GetMapping("/board/event")
+	public String eventpage() {
+		return "board/event";
+	}
+	@GetMapping("/manager/item")
+	public String manager() {
+		return "manager/manager";
+	}
+	
+}
---
+++ src/test/java/com/green/shoes/GreenshoesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.green.shoes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreenshoesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
